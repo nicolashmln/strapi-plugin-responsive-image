89eac7fd4ba929a9ab4895bbe10fbcc1852fb564
Nicolas Hamelin <contact@nicolashamelin.fr>
2021-05-16 14:29:34 +1100

    Fix notification warning

--- admin/src/containers/Settings/index.js
+++ admin/src/containers/Settings/index.js
@@ -68,10 +68,16 @@ const SettingsPage = () => {
         });
       }
 
-      strapi.notification.success('notification.form.success.fields');
+      strapi.notification.toggle({
+        type: 'success',
+        message: {
+          id: 'notification.form.success.fields',
+          defaultMessage: 'Changes Saved'
+        }
+      });
     } catch (err) {
       console.error(err);
-      strapi.notification.error(getTrad('settings.error'));
+      strapi.notification.toggle({ type: 'error', message: getTrad('settings.error') });
     }
   };
 
