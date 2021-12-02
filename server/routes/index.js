439c973e08543a6856e3e4c345b86c74998e9b29
Nicolas Hamelin <contact@nicolashamelin.fr>
2021-12-02 21:39:49 +1100

    Update indexes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,14 @@
-module.exports = {
-  routes: [
-    {
-      method: 'GET',
-      path: '/settings',
-      handler: 'responsive-image.getSettings',
-      config: { policies: [] }
-    },
-    {
-      method: 'PUT',
-      path: '/settings',
-      handler: 'responsive-image.updateSettings',
-      config: { policies: [] }
-    }
-  ]
-}
+module.exports = [
+  {
+    method: 'GET',
+    path: '/settings',
+    handler: 'responsiveImage.getSettings',
+    config: { policies: [] }
+  },
+  {
+    method: 'PUT',
+    path: '/settings',
+    handler: 'responsiveImage.updateSettings',
+    config: { policies: [] }
+  }
+];
